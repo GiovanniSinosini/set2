90984483dd13565dc85ba51c06f486ac87c89c7b
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-27 05:39:36 +0000

    Total users with set

---
+++ src/application/Program.java
@@ -0,0 +1,54 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.LogEntry;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Enter file full path: ");
+		String path = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			Set<LogEntry> set = new HashSet<>();
+			
+			String line = br.readLine();
+			while (line != null) {
+				
+				String[] fields = line.split(" ");
+				String username = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+			
+				set.add(new LogEntry(username, moment)); // does not add automatically repeated content
+			
+				line = br.readLine();
+			}
+			System.out.println("Total users: " + set.size());
+			
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		
+		sc.close();
+		
+		
+		
+		
+		
+	}
+
+}
